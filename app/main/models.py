581ea0714e6ae1a327bfc57880453e818cb80b9f
FIFSAK <anuar200572@gmail.com>
2023-08-21 22:57:25 +0600

    create main models

--- app/main/models.py
+++ app/main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import os
 
 
+class User(models.Model):
+    phone_or_email = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=50)
+
+
+class File(models.Model):
+    name = models.CharField(max_length=100)
+    extension = models.CharField(max_length=10, blank=True)
+    MIME_type = models.CharField(max_length=50)
+    size = models.PositiveIntegerField()
+    upload_date = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        # Определение расширения файла
+        self.extension = os.path.splitext(self.name)[1].lower().strip('.')
+        super(File, self).save(*args, **kwargs)
