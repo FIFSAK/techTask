761dd8d51c9cd6b97abd208eb06f20d8b26fc775
FIFSAK <anuar200572@gmail.com>
2023-08-29 14:06:32 +0600

    docker works

--- app/app/settings.py
+++ app/app/settings.py
@@ -9,12 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-#5e=2&rlc9h$=+$vsujzek5kdo&@-txc7rk1*(lqsp&e1h#+qi"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
